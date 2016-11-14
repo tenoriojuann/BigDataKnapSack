c8e480143c75cd621069beabcb0df2087c70062f
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2016-11-14 00:34:06 -0500

    Adjusted the input number
    The number of input that is loaded into memory is now 10

--- BigDataKnapSack.cpp
+++ BigDataKnapSack.cpp
@@ -2,11 +2,37 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 // A utility function that returns maximum of two integers
 int max(int a, int b) { return (a > b) ? a : b;}
+
+int knapsack(int items, int capacity, int s[], int weight[], int value[]) {
+
+
+	vector<vector<int> > m(items + 1, vector<int>(capacity + 1, 0));
+	for (int jw = 0; jw <= capacity; jw++) m[0][jw] = 0;
+
+	for (int i = 1; i <= items; i++) {
+		for (int jw = 0; jw <= capacity; jw++) {
+
+			// A case when the new item is more than the current weight limit
+			if (weight[i - 1] > jw)
+				m[i][jw] = m[i - 1][jw];
+
+			// A case for weight[i] < jw
+			else {
+				m[i][jw] = max(m[i - 1][jw], value[i - 1] + m[i - 1][jw - weight[i - 1]]);
+				s[jw] = i;
+			}
+		}
+	}
+
+
+	return m[items][capacity];
+}
 
 // Returns the maximum value that can be put in a knapsack of capacity W
 
@@ -14,42 +40,51 @@ int max(int a, int b) { return (a > b) ? a : b;}
 int main() {
 
 	if (!ifstream("data.txt")) {
-		system("generator.exe 50 893");
+		system("generator.exe 50 50");
 	}
 
 	std::ifstream infile("data.txt");
 
 
 	int items, capacity;
-	int a, b;
+	int a[10];
+	int b[10];
 	infile >> capacity;
 	infile >> items;
 
-	int s[51];
+	int *s = new int[capacity+1];
 
-	vector<vector<int> > bag(items + 1, vector<int>(capacity + 1, 0));
+	vector<vector<int> > MaxValues(50, vector<int>(50,0));
 
 
-	while (infile >> a >> b)
+	int counter = 0;
+	int n = 1;
+	int k = capacity;
+	int position = 0;
+
+	while (infile >> a[counter] && infile >> b[counter])	 // make sure it imports   the numbers into the program
 	{
+		
 
-		for (int i = 1; i <= items; i++) {
-			for (int jw = 0; jw <= capacity; jw++) {
 
-				// A case when the new item is more than the current weight limit
-				if (a > jw)
-					bag[i][jw] = bag[i - 1][jw];
 
-				// A case for weight[i] < jw
-				else {
-					bag[i][jw] = max(bag[i - 1][jw], b + bag[i - 1][jw - a]);
-					s[jw] =  i;
-				}
+		if (counter == 9) {
+			counter = 0;													  
+			MaxValues[0][position] = ( knapsack(10, capacity, s, a, b) );
+			k = capacity;
+			n = 1;
+			while (k) {
+				cout << a[s[k] - 1] << " ";
+				k = k - a[s[k] - 1];
 			}
-		}
-	}
-	cout << bag[items][capacity];
 
+			
+		}
+
+		position++;
+		counter++;
+		
+	}
 
 	std::cout << "Press Enter to Quit";
 
